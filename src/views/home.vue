fa348285f05c6fa54bbed310129f4cc8b6d23c12
Leandra <gerosa33@gmail.com>
2019-10-16 17:31:50 -0300

    Modificación home

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-row align="center">
+            <v-col class="text-center" cols="12">
+            <div class="my-2">
+                <v-btn class="title" color="secondary" x-large>INICIAR <br>NUEVO CHAT</v-btn>
+            </div>
+            
+            </v-col>
+            <v-col class="text-center" cols="12">
+            <div class="my-2">
+                <p class="title">Conversaciones anteriores</p>
+            </div>
+            
+            </v-col>
+            
+        </v-row>
+    </div>
+</template>
